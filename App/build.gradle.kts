8ad2edd64e3b5afb82e255e9df5298727346fd83
KireevaV <kirvik134@yandex.ru>
2026-05-20 23:38:06 +0300

    Add App class

---
+++ App/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("application")
+    id("com.github.ben-manes.versions") version "0.54.0"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
